dddcd14ca2617c6ac305a38fd80661fceecc5d70
Jonathan Andrews <jdotand@gmail.com>
2023-01-24 16:12:12 -0500

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+
+// express app
+const app = express();
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcome to the app" });
+});
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log("listening on port 4000");
+});
+
+process.env;
