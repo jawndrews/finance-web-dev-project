953767fc02bd1fd290b8aab87c8a415bdfad8892
Jonathan Andrews <jdotand@gmail.com>
2023-02-07 22:42:47 -0500

    updated

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import dashboardRoute from "./routes/dashboard.js";
+import membersRoute from "./routes/members.js";
+import paymentsRoute from "./routes/payments.js";
+import invoicesRoute from "./routes/invoices.js";
+
+/*config*/
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/*routes*/
+app.use("/dashboard", dashboardRoute);
+app.use("/members", membersRoute);
+app.use("/payments", paymentsRoute);
+app.use("/invoices", invoicesRoute);
+
+/*mongoose setup*/
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
